a64b73db810fb235db19a3853bbff7da56158776
Waka.Y <104607401+waka320@users.noreply.github.com>
2025-05-21 20:53:20 +0900

    fix: css

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -65,7 +65,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="ja">
+    <html lang="ja" className="dark">
       <head>
         <script
           type="application/ld+json"
