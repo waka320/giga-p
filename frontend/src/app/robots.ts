f33ff45781d3bdb56eb9e1233332e3322433a3f2
Waka.Y <104607401+waka320@users.noreply.github.com>
2025-05-22 23:29:06 +0900

    fix: sitemap

--- frontend/src/app/robots.ts
+++ frontend/src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: [ '/game/play/'], // プレイ中の画面はインデックス不要
     },
-    sitemap: 'https://acro-attack.wakaport.com',
+    sitemap: 'https://acro-attack.wakaport.com/sitemap.xml',
   }
 }
